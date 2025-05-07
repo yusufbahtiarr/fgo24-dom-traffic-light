0dd4e3ac92fdde2a572d25013b0b4f39b65ca847
yusufbahtiarr <yusufbdev@gmail.com>
2025-05-07 20:15:03 +0700

    fix: traffic light with promise infinite loop

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,73 +1,61 @@
 
 const btn = document.querySelectorAll('button');
 
-function trafficLight() {
-  btn[0].classList.add('red');
-  setTimeout(() => {
-    btn[0].classList.remove('red');
-    btn[1].classList.add('yellow');
-
-    setTimeout(() => {
-      btn[1].classList.remove('yellow');
-      btn[2].classList.add('green');
-
-      setTimeout(() => {
-        btn[2].classList.remove('green');
-        trafficLight();
-      }, 3000);
-
-    }, 2000);
-
-  }, 3000);
-}
-
-trafficLight();
-
-
-// function red() {
-//   return new Promise ((resolve, reject) => {
-//     btn[0].classList.add('red');
-//     setInterval (() => {
-//       resolve()
-//     }, 3000)
-//   })
-// }
-// function yellow1() {
-//   return new Promise ((resolve, reject) => {
+// function trafficLight() {
+//   btn[0].classList.add('red');
+//   setTimeout(() => {
 //     btn[0].classList.remove('red');
 //     btn[1].classList.add('yellow');
-//     // btn[2].classList.remove();
-//     setInterval (() => {
-//       resolve()
-//     }, 2000)
-//   })
-// }
-// function green() {
-//   return new Promise ((resolve, reject) => {
-//     btn[1].classList.remove('yellow');
-//     btn[2].classList.add('green');
-//     setInterval (() => {
-//       resolve()
-//     }, 3000)
-//     // red()
-//   })
-// }
-// function reset() {
-//   return new Promise ((resolve, reject) => {
-//     btn[0].classList.remove('red');
-//     btn[1].classList.remove('yellow');
-//     btn[2].classList.remove('green');
-//     setInterval (() => {
-//       resolve()
-//     }, 500)
-//   })
+
+//     setTimeout(() => {
+//       btn[1].classList.remove('yellow');
+//       btn[2].classList.add('green');
+
+//       setTimeout(() => {
+//         btn[2].classList.remove('green');
+//         trafficLight();
+//       }, 3000);
+
+//     }, 2000);
+
+//   }, 3000);
 // }
 
-// async function trafficlight2() {
-//   await red();
-//   await yellow1();
-//   await green();
-//   await reset();
-// }
-// trafficlight2();
+// trafficLight();
+
+// With Promise
+function red() {
+  return new Promise((resolve) => {
+    btn[0].classList.add('red');
+    setTimeout(() => {
+      btn[0].classList.remove('red');
+      resolve();
+    }, 3000);
+  });
+}
+function yellow() {
+  return new Promise((resolve) => {
+    btn[1].classList.add('yellow');
+    setTimeout(() => {
+      btn[1].classList.remove('yellow');
+      resolve();
+    }, 2000);
+  });
+}
+function green() {
+  return new Promise((resolve) => {
+    btn[2].classList.add('green');
+    setTimeout(() => {
+      btn[2].classList.remove('green');
+      resolve();
+    }, 3000);
+  });
+}
+async function trafficlight2() {
+  await red();
+  await yellow();
+  await green();
+  trafficlight2(); 
+}
+trafficlight2();
 
